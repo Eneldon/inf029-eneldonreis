019c2cfe1d3b0fdd8055c2d88637d8e433bdc5b1
Eneldon Reis <eneldon.reis1978@gmail.com>
2021-03-06 09:52:12 -0300

    iniciandolistafuncao

---
+++ L1-q1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int soma();
+
+int main(){
+    int a, b, resultado;
+
+    printf(" Informe pirmeiro numero: ");
+    scanf("%d",&a);
+    printf(" Informe segundo numero: ");
+    scanf("%d",&b);
+
+    resultado = soma(a, b);
+
+    printf(" A soma entre %d e %d = %d \n",a,b,resultado);
+
+}
+
+int soma(int x , int y){
+    int res = x + y ;
+    return res;
+
+}
