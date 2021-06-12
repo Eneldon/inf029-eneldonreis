033fbd73d6d743846166a3b76bfcc66c245a89a7
Eneldon Reis <eneldon.reis1978@gmail.com>
2021-06-12 17:55:15 -0300

    atv15

---
+++ Trabalho3/Recursiva15.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+
+int q15(int n);
+
+int main(){
+
+    int numero;
+    int resposta;
+
+    printf("Digite um numero: ");
+    scanf("%d",&numero);
+
+    resposta = q15(numero);
+
+  //  printf(" %d ",resposta);
+
+    return 0;
+
+}
+
+int q15(int n){
+       
+       if(n > 0){ 
+           if(n%2==0){
+           printf(" %d ",n);
+           }
+           q15(n-1);
+       }else{
+           return 0;
+       }
+    
+    
+}
